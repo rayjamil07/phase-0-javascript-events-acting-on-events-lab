1ff9ed5e0b2038fbdd94a272c85892e2c310d26b
rayjamil <ryanwekesa019@gmail.com>
2024-12-15 22:37:33 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = "#000000";
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+document.addEventListener('keydown',function(event){
+    console.log(event);
+})
+
+document.addEventListener('keydown', function(event){
+    if(event.key === "ArrowLeft"){
+        const leftNUmbers = dodger.style.left.replaace('px', '');
+        const left = parseInt(leftNUmbers, 10);
+
+        dodger.style.left = `${left - 1}px`;
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
